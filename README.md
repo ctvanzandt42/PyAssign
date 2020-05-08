2f4c7abf3dbddb1bcaf2cf23fe1a68b2e7c81ca3
Curtis Vanzandt <ctvanzandt42@gmail.com>
2020-05-07 20:00:18 -0500

    Finishing configurations

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>PyAssign</h1>
+
+<p>A small library with only one accessible method. 
+This is here mostly just to get experience pushing something to pip.</p>
--- setup.py
+++ setup.py
@@ -8,12 +8,9 @@ setup(
   author = 'Curtis Vanzandt',                   # Type in your name
   author_email = 'ctvanzandt42@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ctvanzandt42',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ctvanzandt42/PyAssign/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['Assign', 'PyAssign', 'SecretSanta'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
